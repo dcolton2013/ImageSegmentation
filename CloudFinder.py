94810fc549fb2be5386ee76499c67cdf3e34b3d3
Donovan Colton <dono_colto2013@yahoo.com>
2017-11-03 05:37:51 -0400

    fixed some typos causing errors

--- CloudFinder.py
+++ CloudFinder.py
@@ -33,8 +33,8 @@ def cloudyImages(cloudy,path,mask):
             cloudySP.append(resized_img[mask==255])
             #######
             
-            plt.figure(file)
-            plt.imshow(resized_img)
+            #plt.figure(file)
+            #plt.imshow(resized_img)
             
         except Exception as e:
             #error reading image
@@ -55,8 +55,8 @@ def clearImages(clear,path,mask):
             clearSP.append(resized_img[mask==255])
             #######
             
-            plt.figure(file)
-            plt.imshow(img)
+            #plt.figure(file)
+            #plt.imshow(img)
             
         except Exception as e:
             #error reading image
@@ -82,20 +82,20 @@ mask7371 = misc.imresize(mask7371,(70,80),interp = 'nearest')
 path_cloudy_7371 = os.path.join(os.getcwd(), 'Cameras/7371/cloudy/')
 path_clear_7371 = os.path.join(os.getcwd(), 'Cameras/7371/clear/')
 
-#cloudyImages(cloudy7371,path_cloudy_7371,mask7371)
-#clearImages(clear7371,path_clear_7371,mask7371)
+cloudyImages(cloudy7371,path_cloudy_7371,mask7371)
+clearImages(clear7371,path_clear_7371,mask7371)
 ##############
 
 ############## 9112
 cloudy9112 = [f for f in os.listdir("./Cameras/9112/cloudy") if os.path.exists]
 clear9112 = [f for f in os.listdir("./Cameras/9112/clear") if os.path.exists]
-mask9112 = misc.imread('./Cameras/10870/mask/9112.png')
+mask9112 = misc.imread('./Cameras/9112/mask/9112.png')
 mask9112 = misc.imresize(mask9112,(70,80),interp = 'nearest')
-path_cloudy_10870 = os.path.join(os.getcwd(), 'Cameras/9112/cloudy/')
-path_clear_10870 = os.path.join(os.getcwd(), 'Cameras/9112/clear/')
+path_cloudy_9112 = os.path.join(os.getcwd(), 'Cameras/9112/cloudy/')
+path_clear_9112 = os.path.join(os.getcwd(), 'Cameras/9112/clear/')
 
-#cloudyImages(cloudy9112,path_cloudy_9112,mask9112)
-#clearImages(clear9112,path_clear_9112,mask9112)
+cloudyImages(cloudy9112,path_cloudy_9112,mask9112)
+clearImages(clear9112,path_clear_9112,mask9112)
 ##############
 
 ############## 10870
@@ -106,20 +106,20 @@ mask10870 = misc.imresize(mask10870,(70,80),interp = 'nearest')
 path_cloudy_10870 = os.path.join(os.getcwd(), 'Cameras/10870/cloudy/')
 path_clear_10870 = os.path.join(os.getcwd(), 'Cameras/10870/clear/')
 
-#cloudyImages(cloudy10870,path_cloudy_10870,mask10870)
-#clearImages(clear10870,path_clear_10870,mask10870)
+cloudyImages(cloudy10870,path_cloudy_10870,mask10870)
+clearImages(clear10870,path_clear_10870,mask10870)
 ##############
 
 ############## 19106
 cloudy19106 = [f for f in os.listdir("./Cameras/19106/cloudy") if os.path.exists]
 clear19106 = [f for f in os.listdir("./Cameras/19106/clear") if os.path.exists]
-mask19106 = misc.imread('./Cameras/10870/mask/19106.png')
+mask19106 = misc.imread('./Cameras/19106/mask/19106.png')
 mask19106 = misc.imresize(mask19106,(70,80),interp = 'nearest')
 path_cloudy_19106 = os.path.join(os.getcwd(), 'Cameras/19106/cloudy/')
 path_clear_19106 = os.path.join(os.getcwd(), 'Cameras/19106/clear/')
 
-#cloudyImages(cloudy19106,path_cloudy_19106,mask19106)
-#clearImages(clear19106,path_clear_19106,mask19106)
+cloudyImages(cloudy19106,path_cloudy_19106,mask19106)
+clearImages(clear19106,path_clear_19106,mask19106)
 ##############
 
 
