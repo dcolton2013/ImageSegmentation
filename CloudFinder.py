0b3cfd6997cdf0f970170f39f42d8261204a5140
Donovan Colton <dono_colto2013@yahoo.com>
2017-11-22 14:32:40 -0500

    method 2, evaluation

--- CloudFinder.py
+++ CloudFinder.py
@@ -14,15 +14,14 @@ import os, sys
 import matplotlib.pyplot as plt
 import time
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 
-#sky pixels
 #150 images
 cloudy_train_X = np.empty((0,3))
 cloudy_train_y = np.empty((0,1))
 #131 images
 clear_train_X  = np.empty((0,3))
 clear_train_y = np.empty((0,1))
-
 
 cloudy_test_X = np.empty((0,3))
 cloudy_test_y = np.empty((0,1))
@@ -46,7 +45,7 @@ clear_test_y = np.empty((0,1))
 #        #avgs.append(float( avg/ np.shape( array[i] )[0] ))
 #    return avgs       
         
-def cloudyImages(cloudy,path,mask):
+def createDataset(cloudy,path,mask):
     temp_X = np.empty((0,3))
     
     for file in cloudy:
@@ -67,32 +66,9 @@ def cloudyImages(cloudy,path,mask):
             
         except Exception as e:
             #error reading image
-            print(e)
-            print("here")
-    
+            print(e)    
     return temp_X
                
-def clearImages(clear,path,mask):
-    temp_X  = np.empty((0,3))
-    for file in clear:
-        try:
-            if (file == ".DS_Store"):
-                continue
-            #print (file)
-            img = misc.imread(path+file)
-            resized_img = misc.imresize(img,(70,80)) 
-            resized_img.flatten();
-                               
-            temp_X = np.concatenate( (temp_X,resized_img[mask==255] ) )
-            
-            #plt.figure(file)
-            #plt.imshow(img)
-            
-        except Exception as e:
-            #error reading image
-            print(e)
-    return temp_X
-
 ############## 4232
 cloudy4232 = [f for f in os.listdir("./Cameras/4232/cloudy") if os.path.exists]
 clear4232 = [f for f in os.listdir("./Cameras/4232/clear") if os.path.exists]
@@ -101,8 +77,8 @@ mask4232 = misc.imresize(mask4232,(70,80),interp = 'nearest')
 path_cloudy_4232 = os.path.join(os.getcwd(), 'Cameras/4232/cloudy/')
 path_clear_4232 = os.path.join(os.getcwd(), 'Cameras/4232/clear/')
 
-cloudy_train_X= np.concatenate( (cloudy_train_X,cloudyImages(cloudy4232,path_cloudy_4232,mask4232) ))
-clear_train_X = np.concatenate( (clear_train_X,clearImages(clear4232,path_clear_4232,mask4232) ))
+cloudy_train_X= np.concatenate( (cloudy_train_X,createDataset(cloudy4232,path_cloudy_4232,mask4232) ))
+clear_train_X = np.concatenate( (clear_train_X,createDataset(clear4232,path_clear_4232,mask4232) ))
 ##############        
 
 ############## 7371
@@ -113,8 +89,8 @@ mask7371 = misc.imresize(mask7371,(70,80),interp = 'nearest')
 path_cloudy_7371 = os.path.join(os.getcwd(), 'Cameras/7371/cloudy/')
 path_clear_7371 = os.path.join(os.getcwd(), 'Cameras/7371/clear/')
 
-cloudy_train_X = np.concatenate( (cloudy_train_X,cloudyImages(cloudy7371,path_cloudy_7371,mask7371) ))
-clear_train_X = np.concatenate( (clear_train_X,clearImages(clear7371,path_clear_7371,mask7371) ))
+cloudy_train_X = np.concatenate( (cloudy_train_X,createDataset(cloudy7371,path_cloudy_7371,mask7371) ))
+clear_train_X = np.concatenate( (clear_train_X,createDataset(clear7371,path_clear_7371,mask7371) ))
 ##############
 
 ############## 9112
@@ -125,8 +101,8 @@ mask9112 = misc.imresize(mask9112,(70,80),interp = 'nearest')
 path_cloudy_9112 = os.path.join(os.getcwd(), 'Cameras/9112/cloudy/')
 path_clear_9112 = os.path.join(os.getcwd(), 'Cameras/9112/clear/')
 
-cloudy_train_X = np.concatenate( (cloudy_train_X,cloudyImages(cloudy9112,path_cloudy_9112,mask9112) ))
-clear_train_X = np.concatenate( (clear_train_X,clearImages(clear9112,path_clear_9112,mask9112) ))
+cloudy_train_X = np.concatenate( (cloudy_train_X,createDataset(cloudy9112,path_cloudy_9112,mask9112) ))
+clear_train_X = np.concatenate( (clear_train_X,createDataset(clear9112,path_clear_9112,mask9112) ))
 ##############
 
 ############## 10870
@@ -137,8 +113,8 @@ mask10870 = misc.imresize(mask10870,(70,80),interp = 'nearest')
 path_cloudy_10870 = os.path.join(os.getcwd(), 'Cameras/10870/cloudy/')
 path_clear_10870 = os.path.join(os.getcwd(), 'Cameras/10870/clear/')
 
-cloudy_train_X = np.concatenate( (cloudy_train_X,cloudyImages(cloudy10870,path_cloudy_10870,mask10870) ))
-clear_train_X = np.concatenate( (clear_train_X,clearImages(clear10870,path_clear_10870,mask10870) ))
+cloudy_train_X = np.concatenate( (cloudy_train_X,createDataset(cloudy10870,path_cloudy_10870,mask10870) ))
+clear_train_X = np.concatenate( (clear_train_X,createDataset(clear10870,path_clear_10870,mask10870) ))
 ##############
 
 ############## 19106
@@ -149,8 +125,8 @@ mask19106 = misc.imresize(mask19106,(70,80),interp = 'nearest')
 path_cloudy_19106 = os.path.join(os.getcwd(), 'Cameras/19106/cloudy/')
 path_clear_19106 = os.path.join(os.getcwd(), 'Cameras/19106/clear/')
 
-cloudy_test_X = np.concatenate( (cloudy_test_X,cloudyImages(cloudy19106,path_cloudy_19106,mask19106) )) 
-clear_test_X = np.concatenate( (clear_test_X,clearImages(clear19106,path_clear_19106,mask19106) ))
+cloudy_test_X = np.concatenate( (cloudy_test_X,createDataset(cloudy19106,path_cloudy_19106,mask19106) )) 
+clear_test_X = np.concatenate( (clear_test_X,createDataset(clear19106,path_clear_19106,mask19106) ))
 ##############
 
 cloudy_train_y = np.ones(np.shape(cloudy_train_X)[0])
@@ -158,16 +134,36 @@ clear_train_y = np.zeros(np.shape(clear_train_X)[0])
 Xtrain = np.concatenate((cloudy_train_X,clear_train_X))
 ytrain = np.concatenate((cloudy_train_y,clear_train_y))
 
-
-clf = svm.SVC(verbose=True)
-clf.fit(Xtrain[::100, :],ytrain[::100])   
-#   METHOD 1 #############################  
-#   testing -- created testX at line 152
-## initial method one, working on implementing different features to improve percent error
-#
 cloudy_test_y = np.ones(np.shape(cloudy_test_X)[0])
 clear_test_y = np.zeros(np.shape(clear_test_X)[0])
 
 Xtest = np.concatenate((cloudy_test_X,clear_test_X))
 ytest = np.concatenate((cloudy_test_y, clear_test_y))
-yPredictions = clf.predict(Xtest) 
+ 
+#   METHOD 1 #############################  
+## initial method one, working on implementing different features to improve percent error
+##rougly random guesser 48% error
+clf_svm = svm.SVC(verbose=True)
+clf_svm.fit(Xtrain[::100, :],ytrain[::100])  
+yPredictionsSVM = clf_svm.predict(Xtest) 
+errorsSVM = np.abs(ytest - yPredictionsSVM)
+peSVM = np.sum(np.abs(ytest - yPredictionsSVM))/np.size(ytest)
+
+#   METHOD 2 ############################
+##  initial feature,     43% error
+######################################
+clf_dt = DecisionTreeClassifier(random_state = 0)
+clf_dt.fit(Xtrain[::100, :],ytrain[::100])
+yPredictionsDT = clf_dt.predict(Xtest)
+#yPredTree = np.reshape(yPredictionsDT, (1,-1)).transpose()
+errorsDT = np.abs(ytest - yPredictionsDT)
+peDT = np.sum(np.abs(ytest - yPredictionsDT))/np.size(ytest)
+
+# EVALUATION ########################
+print("\n\nSVM percent error: ", peSVM)
+print("Decision Tree Classifier percent error: ", peDT)
+print("The better predictor for our data: ",end = "")
+if (peSVM < peDT):
+    print("SVM")
+else:
+    print("Decision Tree Classifier")
